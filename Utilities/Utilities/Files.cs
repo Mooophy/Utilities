026a711b3a0623169286ce06c582f95fc7b8249b
moophy <MoophyAlan@gmail.com>
2017-03-02 23:12:41 +1300

    updated

--- Utilities/Utilities/Files.cs
+++ Utilities/Utilities/Files.cs
@@ -6,15 +6,15 @@ namespace Utilities
 {
     public static class Files
     {
-        public static IEnumerable<string> CopyLatest(string sourceDirectory, string targetDirectory)
+        public static IEnumerable<string> CopyLatest(string from, string to)
         {
-            var targets = new DirectoryInfo(targetDirectory).GetFiles();
-            var filesToCopy = new DirectoryInfo(sourceDirectory)
+            var targets = new DirectoryInfo(to).GetFiles();
+            var filesToCopy = new DirectoryInfo(from)
                 .GetFiles()
                 .Where(s => targets.Any(t => t.Name == s.Name) && targets.First(t => t.Name == s.Name).LastWriteTime < s.LastWriteTime)
                 .ToList();
             filesToCopy
-                .ForEach(s => File.Copy(s.FullName, Path.Combine(targetDirectory, s.Name), true));
+                .ForEach(s => File.Copy(s.FullName, Path.Combine(to, s.Name), true));
             return filesToCopy.Select(f => f.Name);
         }
     }
